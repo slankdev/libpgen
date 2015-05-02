222d8aca0e5daf9fb819bd5b18add950dc14d929
slank-x250 <slank.dev@gmail.com>
2015-05-02 14:59:06 +0900

    some codes edited at 2015.05.02-14:59

--- src/main.cc
+++ src/main.cc
@@ -2,30 +2,37 @@
 
 const char* dev = "wlan0";
 
-void udp(){
+pgent_mac  macsrc = pgen_getMAC(dev);
+pgent_mac  macdst = "ff:ff:ff:ff:ff:ff";
+pgent_ip ipsrc = pgen_getIP(dev);
+pgent_ip ipdst = "192.168.179.18";
+int sport = 8888;
+int dport = 9999;
+
+
+
+void udp(){//[[[
 	pgen_udp p;
 
-	p.ip_srcIp = pgen_getIP(dev);
-	p.ip_dstIp = "192.168.0.1";
-	p.udp_srcPort = 8888;
-	p.udp_dstPort = 9999;
-
+	p.ip_srcIp = ipsrc;
+	p.ip_dstIp = ipdst;
+	p.udp_srcPort = sport;
+	p.udp_dstPort = dport;
 
 	p.sendPack(dev);
 	p.info();
 	p.hex();
-}
+}//]]]
 
 void tcp(){
 	pgen_tcp p;
 
-	p.ip_srcIp = pgen_getIP(dev);
-	p.ip_dstIp = "192.168.0.18";
-	p.tcp_srcPort = 8888;
-	p.tcp_dstPort = 80;
+	p.ip_srcIp = ipsrc;
+	p.ip_dstIp = ipdst;
+	p.tcp_srcPort = sport;
+	p.tcp_dstPort = dport;
 	p.tcp_frag.syn = 1;
 	p.tcp_frag.ack = 1;
-
 
 	p.sendPack(dev);
 	p.info();
@@ -35,9 +42,8 @@ void tcp(){
 void ip(){//[[[
 	pgen_ip p;
 
-	p.ip_srcIp = pgen_getIP(dev);
-	p.ip_srcIp = "192.168.0.1";
-	p.ip_dstIp = "192.168.0.18";
+	p.ip_srcIp = ipsrc;
+	p.ip_dstIp = ipdst;
 
 	p.sendPack(dev);
 	p.info();
@@ -47,12 +53,12 @@ void ip(){//[[[
 void arp(){//[[[
 	pgen_arp p;
 
-	p.eth_srcEth = pgen_getMAC(dev);
+	p.eth_srcEth = macsrc;
 	p.eth_dstEth = "ff:ff:ff:ff:ff:ff";
-	p.arp_srcEth = pgen_getMAC(dev);
-	p.arp_srcIp  = pgen_getIP(dev);
+	p.arp_srcEth = macsrc;
+	p.arp_srcIp  = ipsrc;
 	p.arp_dstEth = "ff:ff:ff:ff:ff:ff";
-	p.arp_dstIp  = "192.168.0.1";
+	p.arp_dstIp  = ipdst;
 	p.arp_option = PGEN_ARPOP_REQEST;
 		
 	p.sendPack(dev);
@@ -63,17 +69,14 @@ void arp(){//[[[
 void icmp(){//[[[ 
 	pgen_icmp p;
 	
-	p.ip_srcIp = pgen_getIP(dev);
-	p.ip_srcIp = "192.168.0.18";
-	p.ip_dstIp = "192.168.0.1";
+	p.ip_srcIp = ipsrc;
+	p.ip_dstIp = ipdst;
 	p.icmp_option = PGEN_ICMPOP_ECHO;
 	p.icmp_code   = PGEN_ICMPCODE_NET_UNREACH;
 
 	p.sendPack(dev);
 	p.info();
 	p.hex();
-
-
 }//]]]
 
 int main(){
@@ -86,7 +89,7 @@ int main(){
 	//arp();
 	//ip();
 	//icmp();
-	tcp();
+	//tcp();
 	//udp();
 }
 
