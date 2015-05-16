0218b03ae4bf25542a3be7eeec81125d4de4c903
slank-x250 <slank.dev@gmail.com>
2015-05-16 22:47:46 +0900

    some codes edited at 2015.05.16-22:47

--- sample/ARPSPOOF/main.cc
+++ sample/ARPSPOOF/main.cc
@@ -4,20 +4,17 @@ const char* dev = "wlan0";
 
 ipaddr target = "192.168.0.1";
 
-
 int main(){
 	pgen_arp pack;
 
 	pack.ETH.src = pgen_getMAC(dev);
 	pack.ETH.dst = "ff:ff:ff:ff:ff:ff";
 	pack.ARP.srcEth = pgen_getMAC(dev);
-	//pack.ARP.srcIp  = pgen_getIP(dev);
-	//pack.ARP.srcIp  = ;
+	pack.ARP.srcIp  = pgen_getIP(dev);
 	pack.ARP.dstEth = "ff:ff:ff:ff:ff:ff";
 	pack.ARP.dstIp  = target;
 	pack.ARP.option = PGEN_ARPOP_REQEST;
 		
-	pack.sendPack(dev);
 	pack.info();
 	pack.hex();
 
