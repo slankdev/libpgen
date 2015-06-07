1d59bef127138edabf7aec4bbf066c5c60e4cbc5
slank-x250 <slank.dev@gmail.com>
2015-06-07 13:52:00 +0900

    some codes edited at 2015.06.07-13:51

---
+++ sample/test/main.cc
@@ -0,0 +1,20 @@
+#include <pgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char* dev = "wlan0";
+
+
+int main(){
+	pgen_tcp pack;
+	
+	pack.IP.src.setipbydev(dev);
+	pack.IP.dst = "10.128.7.167";
+	pack.TCP.srcPort = 23456;
+	pack.TCP.dstPort = 8080;
+	pack.TCP.frag.syn = 1;
+	
+	pack.INFO();
+	pack.SEND(dev);
+}
