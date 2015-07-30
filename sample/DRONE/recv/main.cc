ef16fbc87c86deb049ef63d1fd273247d7586bb1
slank@x250 <slank.dev@gmail.com>
2015-07-30 12:33:16 +0900

    some codes edited at 2015.07.30-12:33

---
+++ sample/DRONE/recv/main.cc
@@ -0,0 +1,39 @@
+#include <pgen.h>
+#include <stdio.h>
+#include <sniff.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <net/ethernet.h>
+
+const char* dev = "wlan0";
+
+
+bool callback(const u_char* packet, int len){
+
+	pgen_unknown buf(packet, len);
+	pgen_udp udp;
+	pgen_ardrone pack;
+	
+	/*
+	if(buf.isUDP()) printf("UDP");
+	if(buf.isTCP()) printf("TCP");
+	if(buf.isICMP()) printf("ICMP");
+	*/
+	
+
+	if(buf.isUDP()){
+		udp.CAST(packet, len);
+		if(udp.UDP.src == 5556){
+			pack.CAST(packet, len);	
+
+			pack.DSUMMARY();
+		}
+	}
+
+
+	return true;
+}
+
+int main(int argc,char** argv){
+	sniff(dev, callback);
+}
