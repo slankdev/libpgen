9f9315e8ce99a38e063bf0047e32076900cc778c
slank@x250 <slank.dev@gmail.com>
2015-09-05 17:30:34 +0900

    some codes edited at 2015.09.05-17:30

--- sample/ICMP/test2/main.cc
+++ sample/ICMP/test2/main.cc
@@ -3,13 +3,16 @@
 const char* file = "file.pcap";
 
 int main(int argc, char** argv){
+	char data[] = "0123456789";
+
 	pgen_icmp pack;
 	pack.IP.src = "192.168.100.1";
 	pack.IP.dst = "192.168.100.2";
 	pack.ICMP.option = 8;
 	pack.ICMP.code   = 0;
-
+	pack.addData(data, sizeof(data));
 
 	pack.summary();
 	pack.hex();
+
 }
