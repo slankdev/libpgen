5bd872c31380da40dfcbde2466d452e3ee38c098
slank-arch <slank.dev@gmail.com>
2015-10-07 09:01:07 +0900

    add sample check

---
+++ sample/TOOL/check/main.cc
@@ -0,0 +1,54 @@
+
+
+
+#include <pgen.h>
+
+int c = 0;
+
+
+int check(const char* file, pgen_packet* pack){
+	int count  = 0;
+	int result = 0;
+	int len;
+	u_char buf[10000];
+
+	pgen_t* h = pgen_open_offline(file, 0);
+	if(h == NULL){
+		pgen_perror("pgen_open_offline");
+		return -1;
+	}
+	
+	while((len = pgen_recv_from_pcap(h->offline.fd, buf, sizeof(buf))) >= 0){
+		pack->cast(buf, len);
+		pack->compile();
+
+		if(len != pack->len){
+			printf("length not same \n");	
+
+		}else if((memcmp(pack->data, buf, len)!=0)){
+			pack->hex();
+			pgen_hex(buf, len);
+
+		}else{
+			printf("OK \n");
+
+		}
+		count++;
+	}
+
+	printf("%d/%d is false \n", result, count);
+	pgen_close(h);
+	return result;
+}
+
+
+
+
+int main(int argc, char** argv){
+	if(argc != 2){
+		printf("Usage: %s filename \n", argv[0]);
+		return -1;
+	}
+	pgen_arp pack;
+	check(argv[1], &pack);
+}
