b9d7df0ddfbaad16e2cfd00918f23f20817b5418
slank-osx <slank.dev@gmail.com>
2016-02-15 13:56:09 +0900

    [CLEAN] some codes edited at 2016.02.15-13:56

--- include/pgen2/util.h
+++ include/pgen2/util.h
@@ -9,14 +9,18 @@ namespace util {
 
 void hex(const void* buffer, size_t bufferlen);
 
+
+// int    open_netif(const char* ifname);
+// FILE*  open_pcap(const char* filename, mode);
+// FILE*  open_pcapng(const char* filename, mode);
+
 // size_t send_to_netif(int fd, const void* buffer, size_t bufferlen);
 // size_t send_to_pcap(FILE* fd, const void* buffer, size_t bufferlen);
 // size_t send_to_pcapng(FILE* fd, const void* buffer, size_t bufferlen);
-//
+
 // size_t recv_from_netif(int fd, void* buffer, size_t bufferlen);
 // size_t recv_from_pcap(FILE* fd, void* buffer, size_t bufferlen);
 // size_t recv_from_pcapng(FILE* fd, void* buffer, size_t bufferlen);
-
 
 
 } /* namespace util */
