6fa9a282bc9e8d18173144a70665d04ad30bcaef
slank-osx <slank.dev@gmail.com>
2016-02-17 12:29:50 +0900

    [IMPLEMENT] implement pgen::arch::getipv4bydev(), next implement pgen::ipaddress::setv4bydev()

--- include/pgen2/arch/arch.h
+++ include/pgen2/arch/arch.h
@@ -16,8 +16,12 @@
 namespace pgen {
 namespace arch {
 
+
 void getmacbydev(const char* dev, uint8_t mac[6]);
-        
+void getipv4bydev(const char* dev, uint8_t ip[4]);
+// void getipv6bydev(const char* dev, uint16_t ip[8]);
+
+
 } /* namespace arch */
 } /* namespace pgen */
 
--- src/arch/arch.cc
+++ src/arch/arch.cc
@@ -7,6 +7,7 @@
 
 #include <pgen2/arch/arch.h>
 #include <pgen2/exception.h>
+#include <pgen2/util.h>
 
 #include <stdio.h>
 #include <errno.h>
@@ -15,7 +16,9 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <net/if.h>
+#include <netinet/in.h>
 #include <ifaddrs.h>
 
 
@@ -43,47 +46,78 @@ void getmacbydev(const char* dev, uint8_t mac[6]) {
         throw pgen::exception("pgen::arch::getmacbydev: Interface name size is too large");
     }
 
-	int sockd;
-	struct ifreq ifr;
-	if ((sockd=socket(AF_INET,SOCK_DGRAM,0)) < 0){
-        throw pgen::exception("pgen::arch::getmacbydev:socket: " + strerr(errno));
-	}
+    int sockd;
+    struct ifreq ifr;
+    if ((sockd=socket(AF_INET,SOCK_DGRAM,0)) < 0){
+        throw pgen::exception("pgen::arch::getmacbydev:socket: " + strerror(errno));
+    }
 
-	ifr.ifr_addr.sa_family = AF_INET;
-	strcpy(ifr.ifr_name, dev);
-	if(ioctl(sockd, SIOCGIFHWADDR, &ifr) < 0){
-	    close(sockd);
-        throw pgen::exception("pgen::arch::getmacbydev:ioctl: " + strerr(errno));
-	}
-	close(sockd);
+    ifr.ifr_addr.sa_family = AF_INET;
+    strcpy(ifr.ifr_name, dev);
+    if(ioctl(sockd, SIOCGIFHWADDR, &ifr) < 0){
+        close(sockd);
+        throw pgen::exception("pgen::arch::getmacbydev:ioctl: " + strerror(errno));
+    }
+    close(sockd);
     memcpy(mac, ifr.ifr_hwaddr.sa_data, 6);
     
-	return ; //success
+    return ; //success
 
 
 #else // for bsd
     
-	struct ifaddrs *ifap, *ifaptr;
+    struct ifaddrs *ifap, *ifaptr;
     unsigned char *ptr;
-	if(getifaddrs(&ifap) != 0){
+    if(getifaddrs(&ifap) != 0){
         throw pgen::exception("pgen::arch::getmacbydev:getifaddrs: undefined error");
-	}
-	for(ifaptr = ifap; ifaptr != NULL; ifaptr = (ifaptr)->ifa_next) {
-		if (!strcmp((ifaptr)->ifa_name, dev) && 
-				(((ifaptr)->ifa_addr)->sa_family == AF_LINK)) {
-			ptr = (unsigned char *)LLADDR((struct sockaddr_dl *)(ifaptr)->ifa_addr);
+    }
+    for(ifaptr = ifap; ifaptr != NULL; ifaptr = (ifaptr)->ifa_next) {
+        if (!strcmp((ifaptr)->ifa_name, dev) && 
+                (((ifaptr)->ifa_addr)->sa_family == AF_LINK)) {
+            ptr = (unsigned char *)LLADDR((struct sockaddr_dl *)(ifaptr)->ifa_addr);
             memcpy(mac, ptr, 6);
-			freeifaddrs(ifap);
-			return; // success
-		}
-	}
+            freeifaddrs(ifap);
+            return; // success
+        }
+    }
     
 
 #endif
     throw pgen::exception("pgen::arch::getmacbydev: Unknown error");
 }
 
-        
+ 
+
+void getipv4bydev(const char* dev, uint8_t ip[4]) {
+    if(strlen(dev) >= IFNAMSIZ) {
+        throw pgen::exception("pgen::arch::getmacbydev: Interface name size is too large");
+    }
+
+    int sockd;
+    struct ifreq ifr;
+    struct sockaddr_in *sa;
+
+    if ((sockd=socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+        std::string errmsg = "pgen::arch::getipv4bydev:socket: ";
+        errmsg += strerror(errno);
+        throw pgen::exception(errmsg);
+    }
+    ifr.ifr_addr.sa_family = AF_INET;
+    strcpy(ifr.ifr_name, dev);
+    if(ioctl(sockd, SIOCGIFADDR, &ifr) < 0){
+        close(sockd);
+        std::string errmsg = "pgen::arch::getipv4bydev:ioctl: ";
+        errmsg += strerror(errno);
+        throw pgen::exception(errmsg);
+    }
+    close(sockd);
+    sa = (struct sockaddr_in*)&ifr.ifr_addr;
+    memcpy(ip, &(sa->sin_addr.s_addr), sizeof(uint32_t));
+}
+
+
+
+
 } /* namespace arch */
 } /* namespace pgen */
 
