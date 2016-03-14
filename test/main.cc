2de817cb88a394fe6c5e74c72df977be2238a68e
slank-osx <slank.dev@gmail.com>
2016-03-14 10:56:57 +0900

    [ADD] test dir

---
+++ test/main.cc
@@ -0,0 +1,41 @@
+
+
+
+#include <pgen2.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+const char* dev = "en0";
+const char* out = "out.pcap";
+
+
+void callback(const void* buffer, size_t bufferlen) {
+    pgen::packet_type t = pgen::module::detect(buffer, bufferlen); 
+    if (t == pgen::packet_type::arp) {
+        pgen::arp pack(buffer, bufferlen);
+        if (pack.ARP.operation == pgen::arp::operation::reply)
+            pack.ARP.summary(true);
+        // pgen::hex(buffer, bufferlen);
+    }
+}
+
+
+int main() {
+    try {
+
+        pgen::net_stream net(dev, pgen::open_mode::netif);
+        while (1) {
+
+            uint8_t buf[10000];
+            size_t recvlen = net.recv(buf, sizeof buf);
+
+            callback(buf, recvlen);
+        }
+
+
+    } catch (std::exception& e) {
+        printf("%s \n", e.what());
+    }
+}
+
+
