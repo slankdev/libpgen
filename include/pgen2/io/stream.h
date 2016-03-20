2c890dc40c76023d150858ebcafca730e68cf569
slank-arch <slank.dev@gmail.com>
2016-03-20 21:10:35 +0900

    [SUPPORT] linux, send/recv packet to/from network interfaces

--- include/pgen2/io/stream.h
+++ include/pgen2/io/stream.h
@@ -1,11 +1,13 @@
 
 #pragma once
 
-
 #include <stddef.h>
 #include <stdint.h>
 #include <vector>
 #include <stdio.h>
+#include <sys/socket.h> // for struct sockaddr
+
+#include <pgen2/arch/arch.h>
 
 
 
@@ -115,13 +117,16 @@ class pcap_stream : public file_stream {
 class net_stream : public base_stream {
     private:
         int _fd;
+#if defined(__PGEN_OSX)
         std::vector<uint8_t> _buffer;
         uint8_t* _buffer_point;
         ssize_t _buffer_size_readed;
+#endif
 
         void open_netif(const char* name, size_t buffer_size=0);
 
         void ioctl(unsigned long l, void* p);
+        void bind(const struct sockaddr* sa, size_t len);
         void write(const void* buffer, size_t bufferlen);
         size_t read(void* buffer, size_t bufferlen);
     public:
