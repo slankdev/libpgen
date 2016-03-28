4150e25c2190ef2e70e9d5b7473def40c12242d7
slank-arch <slank.dev@gmail.com>
2016-03-28 11:21:45 +0900

    [ADD] definition zundoko_header class

---
+++ sample/zundoko/zundoko.h
@@ -0,0 +1,89 @@
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+
+
+
+struct zundoko {
+    static const uint16_t ZUN     = 0;
+    static const uint16_t DOKO    = 1;
+    static const uint16_t KIYOSHI = 2;
+    uint16_t type;
+    uint16_t message_length;
+};
+
+
+
+
+class Socket {
+    private:
+        int fd;
+
+    public:
+        
+        Socket() {}
+        Socket(int domain, int type, int protocol) {
+            socket(domain, type, protocol);   
+        }
+        ~Socket() {
+            close();
+        }
+
+        void socket(int domain, int type, int protocol) {
+            fd = ::socket(domain, type, protocol);
+            if (fd < 0) {
+                perror("socket");
+                exit(-1);
+            }
+        }
+        void close() {
+            if (fd >= 0) 
+                ::close(fd);
+        }
+        void bind(const struct sockaddr* addr, socklen_t addrlen) {
+            int res = ::bind(fd, addr, addrlen);
+            if (res < 0) {
+                perror("bind");
+                exit(-1);
+            }
+        }
+        void ioctl(unsigned long request, void* arg) {
+            int res = ::ioctl(fd, request, arg);
+            if (res < 0) {
+                perror("ioctl");
+                exit(-1);
+            }
+        }
+        void sendto(const void* message, size_t length, int flag, 
+                const struct sockaddr* dst_addr, socklen_t dst_len) {
+            ssize_t res = ::sendto(fd, message, length, 
+                    flag, dst_addr, dst_len);
+            if (res < 0) {
+                perror("sendto");
+                exit(-1);
+            }
+        }
+        ssize_t recvfrom(void* message, size_t length, int flag, 
+                struct sockaddr* dst_addr, socklen_t* dst_len) {
+            ssize_t res = ::recvfrom(fd, message, length, 
+                    flag, dst_addr, dst_len);
+            if (res < 0) {
+                perror("recvfrom");
+                exit(-1);
+            }
+            return res;
+        }
+
+    
+};
---
+++ src/core/protocol/zundoko.cc
@@ -0,0 +1,14 @@
+
+
+#include <arpa/inet.h>
+#include <pgen2/core/protocol/zundoko.h>
+#include <pgen2/exception.h>
+
+
+namespace pgen {
+
+void ____func(){
+    printf("test\n");
+}
+    
+} /* namespace pgen */
