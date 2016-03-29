2198377e8ee5741ab68032cb02f07b00043000a7
slank-osx <slank.dev@gmail.com>
2016-03-29 13:20:04 +0900

    [UPDATE] zundoko

--- sample/zundoko/main.cc
+++ sample/zundoko/main.cc
@@ -5,10 +5,10 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-const char* dev = "wlan0";
+const char* dev = "en0";
 const char* out = "out.pcapng";
 const char* in  = "in.pcapng";
-
+#define ZDPORT 9988
 using std::cout;
 using std::endl;
 
@@ -16,7 +16,7 @@ int main() {
     try 
     {
 
-        pgen::net_stream net(dev, pgen::open_mode::netif);
+        pgen::net_stream net(in, pgen::open_mode::netif);
         uint8_t buf[10000];
 
         while (1) {
@@ -25,11 +25,14 @@ int main() {
             pgen::packet_type type = pgen::module::detect(buf, recvlen);
             if (type == pgen::packet_type::udp) {
                 pgen::udp udp(buf, recvlen);
-                if (udp.UDP.src==pgen::udp::port::zundoko || udp.UDP.dst==pgen::udp::port::zundoko) {
+                if (udp.UDP.src==ZDPORT || udp.UDP.dst==ZDPORT) {
                     pgen::zundoko pack(buf, recvlen);
                     if (pack.ZUNDOKO.type == pgen::zundoko::type::kiyoshi) {
                         printf("Success %s msg=\"%s\" \n", pack.IP.dst.str().c_str(),
                                 pack.ZUNDOKO.msg.c_str());
+                    } else {
+                        printf("other type \n");
+    
                     }
                 }
             }
